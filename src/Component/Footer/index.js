28af09f0c74a0e601c0ebf46d4facdbf8ef8ef72
Sumit Rajput <ssumitrajput854@gmail.com>
2020-08-06 14:36:03 +0530

    feat: add icons to footer and text options

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -1,12 +1,15 @@
 import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
 import styles from './sender.module.css'
+import 'font-awesome/css/font-awesome.min.css';
 
 const Sender = ({ }) => {
   return (
-      <div className={styles.rootWrapper}>
-        FROM
-      </div>
+    <div className={styles.rootWrapper}>
+      <i className="fa fa-smile-o" />
+      <i className="fa fa-paperclip" />
+      <i className="fa fa-pencil" />
+    </div>
   );
 }
 
--- src/Component/TextOptions/index.js
+++ src/Component/TextOptions/index.js
@@ -1,12 +1,39 @@
 import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
 import styles from './textOptions.module.css'
+import 'font-awesome/css/font-awesome.min.css';
 
 const TextOptions = ({ }) => {
   return (
-      <div className={styles.rootWrapper}>
-        TextOptions
+    <div className={styles.rootWrapper}>
+      <div>
+        <span>
+          Normal text
+        </span>
+        <i className={`fa fa-caret-down ${styles.grayColor}`} />
       </div>
+      <div className={styles.verticalLine}>&nbsp;</div>
+      <div>
+        <i className={`fa fa-bold ${styles.grayColor}`} />
+        <i className={`fa fa-italic ${styles.grayColor}`} />
+        <i className={`fa fa-ellipsis-h ${styles.grayColor}`} />
+      </div>
+      <div className={styles.verticalLine}>&nbsp;</div>
+      <div>
+        <i className={`fa fa-font ${styles.grayColor}`} />
+      </div>
+      <div className={styles.verticalLine}>&nbsp;</div>
+      <div>
+        <i className={`fa fa-list ${styles.grayColor}`} />
+        <i className={`fa fa-list-ol ${styles.grayColor}`} />
+      </div>
+      <div className={styles.verticalLine}>&nbsp;</div>
+      <div>
+        <i className={`fa fa-link ${styles.grayColor}`} />
+        <i className={`fa fa-picture-o ${styles.grayColor}`} />
+        <i className={`fa fa-quote-right ${styles.grayColor}`} />
+      </div>
+    </div>
   );
 }
 
