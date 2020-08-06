471719aad2f884e7b2cfd2297485df21da671311
Sumit Rajput <ssumitrajput854@gmail.com>
2020-08-06 11:14:11 +0530

    feat: add structure for textbox options component

---
+++ src/Component/TextOptions/index.js
@@ -0,0 +1,18 @@
+import React, { useReducer } from 'react';
+import PropTypes from 'prop-types';
+import styles from './textOptions.module.css'
+
+const TextOptions = ({ }) => {
+  return (
+      <div className={styles.rootWrapper}>
+        TextOptions
+      </div>
+  );
+}
+
+TextOptions.propTypes = {
+
+};
+
+
+export default TextOptions
