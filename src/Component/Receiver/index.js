4787dbe2fe7bc93095cbec6bfeccae5e6ef8b354
Sumit Rajput <ssumitrajput854@gmail.com>
2020-08-07 00:53:23 +0530

    feat: add auto complete component to receiver

--- src/Component/Receiver/index.js
+++ src/Component/Receiver/index.js
@@ -1,12 +1,44 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './receiver.module.css'
+import emailList from '../../data.js'
+import Autocomplete from '../Autocomplete';
+import validateEmail from '../../Helper/validateMail'
 
 const Receiver = ({ }) => {
+  const [receivers, setReceiver] = useState([])
+  const mails = emailList.split(',').map((mail) => mail.replace(/(\r\n|\n|\r)/gm, ""))
+
+  const removeMailId = (index) => {
+    const updatedReceivers = receivers.filter((item, id) => id !== index)
+    setReceiver(updatedReceivers)
+  }
+
   return (
-      <div className={styles.toBox}>
-        <span className={styles.title}>To:</span>
+    <div className={styles.toBox}>
+      <span className={styles.title}>To:</span>
+      <div className={styles.receivers}>
+        {receivers.map((mailId, index) => validateEmail(mailId) ?
+          <div className={styles.verifiedWrapper}>
+            <span className={styles.verified}>
+              {mailId}
+            </span>
+            <i className={`fa fa-times ${styles.cross}`} onClick={() => removeMailId(index)} />
+          </div>
+          :
+          <div className={styles.unverifiedWrapper}>
+            <span className={styles.unverified}>
+              {mailId}
+            </span>
+            <i className={`fa fa-exclamation ${styles.exclamation}`} />
+          </div>
+        )}
+        <Autocomplete
+          list={mails}
+          getValue={(value) => setReceiver([...receivers, value])}
+        />
       </div>
+    </div>
   );
 }
 
