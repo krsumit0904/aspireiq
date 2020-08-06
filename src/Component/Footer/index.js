eb49449a6e600fd0fc46e41533f568250a007281
Sumit Rajput <ssumitrajput854@gmail.com>
2020-08-06 11:13:51 +0530

    feat: add structure for mailbox footer component

---
+++ src/Component/Footer/index.js
@@ -0,0 +1,18 @@
+import React, { useReducer } from 'react';
+import PropTypes from 'prop-types';
+import styles from './sender.module.css'
+
+const Sender = ({ }) => {
+  return (
+      <div className={styles.rootWrapper}>
+        FROM
+      </div>
+  );
+}
+
+Sender.propTypes = {
+
+};
+
+
+export default Sender
